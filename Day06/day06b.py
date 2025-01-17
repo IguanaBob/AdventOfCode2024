d8ee273f033148cfab79e9383c73e712bb005bcb
Jon <43802921+IguanaBob@users.noreply.github.com>
2025-01-17 06:48:30 -0600

    Fix licence text

--- Day06/day06b.py
+++ Day06/day06b.py
@@ -15,7 +15,7 @@
 # FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 # details.
 #
-# You should have received a copy of the GNU General Affero Public License
+# You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>. 
 #
 ###############################################################################
