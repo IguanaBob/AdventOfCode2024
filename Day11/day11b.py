bc8a4d748d02bbf2f7cbfd22678ca69af2b7bd30
Jon <43802921+IguanaBob@users.noreply.github.com>
2025-02-16 08:31:29 -0600

    Save progress for Day 11 Part 2

--- Day11/day11b.py
+++ Day11/day11b.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ###############################################################################
 #
-# 2024 Advent of Code, Day 11, Part 1
+# 2024 Advent of Code, Day 11, Part 2
 # Copyright © 2025 Jonathan Hull
 #
 # This program is free software: you can redistribute it and/or modify it under
@@ -19,27 +19,26 @@
 #
 ###############################################################################
 #
-# ChatGPT's summary: The Plutonian Pebbles change with each blink following
-# specific rules: a stone marked 0 becomes 1; if a stone's number has an even
-# number of digits, it splits into two stones with the left half on the left
-# and the right half on the right; otherwise, a stone is replaced by one
-# engraved with its number multiplied by 2024. The order of stones remains
-# preserved, and your task is to determine how many stones there will be after
-# blinking 25 times.
+# ChatGPT's summary:
+# (I didn't include because the only difference is the blink count is now 75)
 #
 # My Reflection:
-# After reading description: Well, seems like just more array manipulation, and
-# probably easier than some of the previous days. No sweat.
-# After solving: The actual list manipulation was fairly easy, just took some
-# tries to figure out the syntax for rule 2, and I had to remember to strip()
+# After reading description: Okay, so I change blinks to 75 and wait...
+# During: ...for 4 hours (34 blinks in)... (cancels and changes to storing as
+# int instead of str) ...and wait...
+# 
+#
+# There may be a faster way, possibly iterating once only but moving backwards
+# after a change, so I may revisit later to try it for fun.
 #
 ###############################################################################
 
 import sys
 
 debug = False
+debug2 = True
 stones = []
-blinks = 25
+blinks = 75
 
 def readfile():
     if (len(sys.argv) > 1):
@@ -50,42 +49,49 @@ def readfile():
     with open(inputfile,"r") as file:
         if debug: print("Reading input...")
         stones = file.readline().strip().split(" ")
+
+    i = 0
+    while ( i < len(stones) ):
+        stones[i] = int(stones[i])
+        i += 1
     return stones
 
-def printstones(stones):
-    print(" ".join(stones))
-    return
+#def printstones(stones):
+#    print(" ".join(stones))
+#    return
 
 def blink(stones):
     pos = 0
     while ( pos < len(stones) ):
-        if debug: print( "Checking pos " + str(pos) + ": " + stones[pos] )
-        if ( stones[pos] == "0" ):
+        if debug: print( "Checking pos " + str(pos) + ": " + str(stones[pos]) )
+        if ( stones[pos] == 0 ):
             # Rule 1: If stone is 0, replaced with 1.
             if debug: print("Found 0, changing to 1")
-            stones[pos] = "1"
-        elif ( len(stones[pos]) % 2 == 0 ):
+            stones[pos] = 1
+        elif ( len(str(stones[pos])) % 2 == 0 ):
             # Rule 2: If stone is even number of digits, replaced by 2 stones. Left stone has left half of digits, right stone has right half of digits, drop leading zeros.
-            half = len(stones[pos])/2
-            leftnum = stones[pos][0:int(half)]
+            half = len(str(stones[pos]))/2
+            leftnum = str(stones[pos])[0:int(half)]
             # Casting to int temporarily is the easiest way to remove leading zeros
-            rightnum = str(int(stones[pos][int(half):]))
+            rightnum = str(int(str(stones[pos])[int(half):]))
             if debug: print("Found even number of digits, splitting stone: " + leftnum + " " + rightnum)
-            stones[pos] = leftnum
+            stones[pos] = int(leftnum)
             pos += 1
-            stones.insert(pos,rightnum)
+            stones.insert(pos,int(rightnum))
         else:
             # Rule 3: Default: Stone replaced by stone with old stone's number * 2024.
             if debug: print("Default rule, multiplying by 2024: " + str(int(stones[pos]) * 2024))
-            stones[pos] = str( int(stones[pos]) * 2024 )
+            stones[pos] = stones[pos] * 2024
         pos += 1
     return stones
 
 stones = readfile()
-printstones(stones)
+#printstones(stones)
 i = 0
 while ( i < blinks ):
     blink(stones)
-    if debug: printstones(stones)
+#    if debug: printstones(stones)
     i += 1
+    if debug2: print("Blinks: " + str(i))
+    if debug2: print("Stones: " + str(len(stones)))
 print("Stones after " + str(blinks) + " blinks: " + str(len(stones)))
